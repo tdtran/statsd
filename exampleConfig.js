e7aeeeadcb93ba884dafdd6a326a870ceebd9021
Mike Stipicevic <stipredirect@gmail.com>
2011-12-08 11:17:29 -0800

    restructuring, add test framework

--- exampleConfig.js
+++ exampleConfig.js
@@ -5,6 +5,7 @@
 //, libratoUser: "<librato email>"
 //, libratoApiKey: "<librato api key>"
 //, libratoSource: "loadbalancer-statsd" // optional source
+//, libratoSnap: 10 // snap measurements to this interval
   , batch: 200
   , port: 8125
 }
