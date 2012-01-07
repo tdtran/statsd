ed1faf7f1ec5124ef5c582b1b2011331983216ed
till <till@php.net>
2012-01-07 19:09:44 +0100

    add other available options to example config

--- exampleConfig.js
+++ exampleConfig.js
@@ -5,6 +5,11 @@
 //, libratoUser: "<librato email>"
 //, libratoApiKey: "<librato api key>"
 //, libratoSource: "loadbalancer-statsd" // optional source
+//, debug: 1
+//, debugInterval: 10000
+//, mgmt_port: 8126
+//, flushInterval: 10000
+//, percentThreshold: 90
   , batch: 200
   , port: 8125
 }
